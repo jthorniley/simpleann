0364aec3a8b843a0870826a8bf8413b0f172156c
James Thorniley <james_thorniley@yahoo.co.uk>
2010-05-21 13:54:33 +0100

    forgot to delete / update these files in last commit

--- examples/basic.c
+++ examples/basic.c
@@ -1,20 +1,25 @@
 #include <math.h>
 #include <stdlib.h>
 
-#include "simpleann.h"
+#include <simpleann/simpleann.h>
 
 int main() {
 
     simple_net net;
+    transfer tr;
+    time_t tm;
+
     double inputs[4];
     inputs[0] = 0.0;
     inputs[1] = 1.0;
     inputs[2] = 2.0;
     inputs[3] = 3.0;
 
-    srand( time() );
+    srand( time(NULL) );
 
-    simple_net_new( &net, &tanh, 3, 4, 2, 2 );
+    transfer_new( &tr, TRANSFER_TANH );
+
+    simple_net_new( &net, tr, 3, 4, 2, 2 );
 
     simple_net_update( &net, inputs );
 
